c4f3c52bc74618584c72aed3d0c770873ef02087
Alvaro Rubina <145699883+Alvaro-Rubina@users.noreply.github.com>
2025-10-11 17:48:22 -0300

    Commit inicial

---
+++ src/test/java/com/example/auth0springbootstarter/Auth0SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth0springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0SpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
