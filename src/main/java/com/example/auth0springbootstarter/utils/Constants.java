7104e10cae7707a22101d6b79c99a44d8501a53a
Alvaro Rubina <145699883+Alvaro-Rubina@users.noreply.github.com>
2025-10-13 17:42:07 -0300

    Creado RoleService y clase Constants para roles predefinidos

---
+++ src/main/java/com/example/auth0springbootstarter/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.example.auth0springbootstarter.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Constants {
+
+    public static final String USER_ROL_NAME = "USUARIO";
+    public static final String ADMIN_ROL_NAME = "ADMIN";
+
+    public static final Map<String, String> ROLES = new HashMap<>() {{
+        put(ADMIN_ROL_NAME, "Rol con permisos extendidos para administradores.");
+        put(USER_ROL_NAME, "Rol con permisos limitados para usuarios.");
+    }};
+}
