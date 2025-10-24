8a1c3d20c3f950c1ddd58c8f1a0637389caafdc9
Alvaro Rubina <alvarorubina132@gmail.com>
2025-10-24 13:35:45 -0300

    Implementado manejo de cliente HTTP y renovación automática de token en Auth0Config

--- src/main/java/com/example/auth0springbootstarter/config/security/Auth0Config.java
+++ src/main/java/com/example/auth0springbootstarter/config/security/Auth0Config.java
@@ -2,12 +2,13 @@ package com.example.auth0springbootstarter.config.security;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.mgmt.ManagementAPI;
+import com.auth0.client.mgmt.SimpleTokenProvider;
 import com.auth0.exception.Auth0Exception;
-import com.auth0.json.auth.TokenHolder;
+import com.auth0.net.client.Auth0HttpClient;
+import com.auth0.net.client.DefaultHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.auth0.net.AuthRequest;
 
 @Configuration
 public class Auth0Config {
@@ -22,10 +23,32 @@ public class Auth0Config {
     private  String clientSecret;
 
     @Bean
-    public ManagementAPI managementAPI() throws Auth0Exception {
-        AuthAPI authAPI = new AuthAPI(domain, clientId, clientSecret);
-        AuthRequest request = authAPI.requestToken("https://" + domain + "/api/v2/");
-        TokenHolder holder = request.execute();
-        return new ManagementAPI(domain, holder.getAccessToken());
+    public Auth0HttpClient auth0HttpClient() {
+        return DefaultHttpClient.newBuilder()
+                .withConnectTimeout(10)
+                .withReadTimeout(10)
+                .build();
     }
+
+    @Bean
+    public AuthAPI authAPI(Auth0HttpClient httpClient) {
+        return AuthAPI.newBuilder(domain, clientId, clientSecret)
+                .withHttpClient(httpClient)
+                .build();
+    }
+
+    @Bean
+    public ManagementAPI managementAPI(Auth0HttpClient httpClient, AuthAPI authAPI) throws Auth0Exception {
+        // obtengo token inicial
+        String apiToken = authAPI.requestToken("https://" + domain + "/api/v2/")
+                .execute()
+                .getBody()
+                .getAccessToken();
+
+        // acá se maneja la renovación automática del token
+        return ManagementAPI.newBuilder(domain, SimpleTokenProvider.create(apiToken))
+                .withHttpClient(httpClient)
+                .build();
+    }
+
 }
